e22ca4396eac14e3464d37805c7939711ec4eeca
Duncan <loxa10077@gmail.com>
2020-02-21 17:07:04 +0800

    crrect route typo

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'courses#index'
-  resource :courses
+  resources :courses, only: [:index]
 
   namespace :admin, path: :make_backend_url_abstruse do
     root 'courses#index'
 
-    resource :courses
-    resource :orders
-    resource :user_courses, only: [:index, :edit, :update]
+    resources :courses
+    resources :orders
+    resources :user_courses, only: [:index, :edit, :update]
   end
 end
