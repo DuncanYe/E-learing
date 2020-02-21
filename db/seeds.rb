6aab7e7218d2d7c8c3e2d2c99e933d74c6c5d8a6
Duncan <loxa10077@gmail.com>
2020-02-21 17:20:25 +0800

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+Category.destroy_all
+
+category_list = [
+  {name: "程式語言"},
+  {name: "音樂"},
+  {name: "教育"},
+  {name: "人文"}
+]
+
+category_list.each do |category|
+  Category.create( name: category[:name])
+  puts "#{category[:name]} has created!"
+end
+
+#Default admin
+User.create(email: "root@example.com", password: "111111", role: "admin")
+puts "Default admin create!"
