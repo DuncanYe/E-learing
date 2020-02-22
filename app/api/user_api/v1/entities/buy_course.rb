eed428a1d9e773dc47a61e1970e2bd84be0b1b99
Duncan <loxa10077@gmail.com>
2020-02-22 10:36:03 +0800

    add buy course spec

--- app/api/user_api/v1/entities/buy_course.rb
+++ app/api/user_api/v1/entities/buy_course.rb
@@ -2,7 +2,9 @@ module UserApi::V1::Entities
   class BuyCourse < Grape::Entity
     expose :course_id, documentation: {type: "String", desc: "課程ID"}
     expose :state, documentation: {type: "String", desc: "是否可用"}
-    expose :end_at, documentation: {type: "String", desc: "使用到何時"}
+    expose :end_at, documentation: {type: "String", desc: "使用到何時"} do |obj|
+      obj.end_at.strftime('%Y-%m-%d %H:%M')
+    end
     expose :amount, documentation: {type: "Integer", desc: "金額"}
     expose :currency, documentation: {type: "String", desc: "幣別"}
     expose :category, documentation: {type: "String", desc: "種類名稱"} do |obj|
--- spec/api/user_api/endpoint_spec.rb
+++ spec/api/user_api/endpoint_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe UserApi::V1::Endpoint, type: :request do
     it '回傳當下時間' do
       get "http://localhost:3000/user_api/v1/ping"
       result = JSON.parse(response.body)
+      expect(response.status).to be 200
       expect(result['data']).to eq(Time.current.iso8601)
     end
   end
